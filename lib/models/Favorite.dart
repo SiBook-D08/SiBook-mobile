53eb946754ae7b7a6b5a5cea5841c384e3de0498
rzapriono <apriono.reza@gmail.com>
2023-12-10 16:11:11 +0700

    add to favorite only should be ok

---
+++ lib/models/Favorite.dart
@@ -0,0 +1,59 @@
+// To parse this JSON data, do
+//
+//     final favorite = favoriteFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Favorite> favoriteFromJson(String str) =>
+    List<Favorite>.from(json.decode(str).map((x) => Favorite.fromJson(x)));
+
+String favoriteToJson(List<Favorite> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Favorite {
+  String model;
+  int pk;
+  Fields fields;
+
+  Favorite({
+    required this.model,
+    required this.pk,
+    required this.fields,
+  });
+
+  factory Favorite.fromJson(Map<String, dynamic> json) => Favorite(
+        model: json["model"],
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "model": model,
+        "pk": pk,
+        "fields": fields.toJson(),
+      };
+}
+
+class Fields {
+  int user;
+  int book;
+  String alasan;
+
+  Fields({
+    required this.user,
+    required this.book,
+    required this.alasan,
+  });
+
+  factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        user: json["user"],
+        book: json["book"],
+        alasan: json["alasan"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "user": user,
+        "book": book,
+        "alasan": alasan,
+      };
+}
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -5,66 +5,67 @@ import 'package:sibook_mobile/widgets/item_card.dart';
 import 'package:sibook_mobile/widgets/left_drawer.dart';
 
 class MyHomePage extends StatelessWidget {
-	MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
-	final List<ShopItem> items = [
-		ShopItem("Lihat Item",  Icons.checklist, const Color(0xFF1A46BD)),
-		ShopItem("Tambah Item", Icons.add_shopping_cart, const Color(0xFF0F286B)),
-		ShopItem("Logout", Icons.logout, const Color(0xFF091840)),
+  final List<ShopItem> items = [
+    ShopItem("Lihat Item", Icons.checklist, const Color(0xFF1A46BD)),
+    ShopItem("Tambah Item", Icons.add_shopping_cart, const Color(0xFF0F286B)),
+    ShopItem("Logout", Icons.logout, const Color(0xFF091840)),
     ShopItem("Borrow", Icons.book, Color.fromARGB(255, 29, 52, 115)),
-	];
+    ShopItem("Favorit", Icons.bookmark, Color.fromARGB(255, 8, 67, 232)),
+  ];
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-		appBar: AppBar(
-			title: const Center(
-			child: Text(
-				'SiBook',
-				style: TextStyle(color: Colors.white),
-			),
-			),
-			backgroundColor: Color.fromARGB(221, 67, 182, 54),
-			foregroundColor: const Color.fromARGB(255, 125, 113, 113),
-		),
-		drawer: const LeftDrawer(title: "Menu"),
-		body: SingleChildScrollView(
-			// Widget wrapper yang dapat discroll
-			child: Padding(
-			padding: const EdgeInsets.all(10.0), // Set padding dari halaman
-			child: Column(
-				// Widget untuk menampilkan children secara vertikal
-				children: <Widget>[
-				const Padding(
-					padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-					// Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
-					child: Text(
-					'Selamat datang di SiBook', // Text yang menandakan toko
-					textAlign: TextAlign.center,
-					style: TextStyle(
-						fontSize: 30,
-						fontWeight: FontWeight.bold,
-					),
-					),
-				),
-				// Grid layout
-				GridView.count(
-					// Container pada card kita.
-					primary: true,
-					padding: const EdgeInsets.all(20),
-					crossAxisSpacing: 10,
-					mainAxisSpacing: 10,
-					crossAxisCount: 3,
-					shrinkWrap: true,
-					children: items.map((ShopItem item) {
-					// Iterasi untuk setiap item
-					return ShopCard(item);
-					}).toList(),
-				),
-				],
-			),
-			),
-		),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'SiBook',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(221, 67, 182, 54),
+        foregroundColor: const Color.fromARGB(255, 125, 113, 113),
+      ),
+      drawer: const LeftDrawer(title: "Menu"),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Selamat datang di SiBook', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ShopItem item) {
+                  // Iterasi untuk setiap item
+                  return ShopCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
