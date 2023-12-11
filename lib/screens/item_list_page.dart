210bf8ab3afe23ca09642e6a064353729435d418
Adrian <adrian.hamzah15@gmail.com>
2023-12-11 15:22:25 +0700

    edited styling for borrow

--- lib/screens/item_list_page.dart
+++ lib/screens/item_list_page.dart
@@ -40,101 +40,149 @@ class _ItemPageState extends State<ItemPage> {
 
   @override
   Widget build(BuildContext context) {
+    Color startColor = Color.fromARGB(255, 204, 207, 209); // Color to fade
+LinearGradient existingGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+          Color.fromARGB(255, 0, 11, 0),
+          Color.fromARGB(255, 78, 117, 151),
+          Color.fromARGB(249, 238, 203, 156)
+  ],
+); // Existing background gradient
+
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 142, 168, 125),
           title: const Text('Item'),
         ),
         drawer: const LeftDrawer(title: 'catalogue'),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: searchController,
-                decoration: InputDecoration(
-                  labelText: 'Search',
-                  hintText: 'Search for items...',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+          Color.fromARGB(255, 0, 11, 0),
+          Color.fromARGB(255, 78, 117, 151),
+          Color.fromARGB(249, 238, 203, 156)
+                  ]),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Search',
+                      hintText: 'Search for items...',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        filteredItems = allItems.where((element) {
+                          return element.fields.title
+                              .toLowerCase()
+                              .contains(value.toLowerCase());
+                        }).toList();
+                      });
+                    },
                   ),
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    filteredItems = allItems.where((element) {
-                      return element.fields.title
-                          .toLowerCase()
-                          .contains(value.toLowerCase());
-                    }).toList();
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: FutureBuilder(
-                  future: fetchItem(),
-                  builder: (context, AsyncSnapshot snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    }
+                Expanded(
+                  child: FutureBuilder(
+                      future: fetchItem(),
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                        if (snapshot.hasError) {
+                          return Center(
+                              child: Text('Error: ${snapshot.error}'));
+                        }
 
-                    allItems = snapshot.data;
-                    List<Product> itemsToShow = searchController.text.isEmpty
-                        ? allItems
-                        : filteredItems;
+                        allItems = snapshot.data;
+                        List<Product> itemsToShow =
+                            searchController.text.isEmpty
+                                ? allItems
+                                : filteredItems;
 
-                    return ListView.builder(
-                      itemCount: itemsToShow.length,
-                      itemBuilder: (_, index) => InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ItemDetailPage(
-                                item: itemsToShow[index],
-                              ),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 16, vertical: 12),
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                itemsToShow[index].fields.title,
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
+                        return ListView.builder(
+                          itemCount: itemsToShow.length,
+                          itemBuilder: (_, index) => InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ItemDetailPage(
+                                    item: itemsToShow[index],
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 10),
-                              Text(itemsToShow[index].fields.author),
-                              const SizedBox(height: 10),
-                              Text(itemsToShow[index]
-                                          .fields
-                                          .description
-                                          .length >
-                                      200
-                                  ? "${itemsToShow[index].fields.description.substring(0, 201)}..."
-                                  : itemsToShow[index].fields.description),
-                              const SizedBox(height: 10),
-                              Text(
-                                  "Banyak Halaman: ${itemsToShow[index].fields.numPages}"),
-                            ],
+                              );
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 12),
+                              padding: const EdgeInsets.all(20.0),
+                              child:
+                                Container(
+                                  decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      startColor.withOpacity(1.0), // Start with full opacity
+                                      startColor.withOpacity(0.0), // Fade to transparent
+                                    ],
+                                  ),
+                                ),
+                                child: ShaderMask(
+  shaderCallback: (Rect bounds) {
+    return existingGradient.createShader(bounds);
+  },
+  blendMode: BlendMode.dstIn,
+                                  child: 
+                                  Padding(padding: EdgeInsets.all(4.0), child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    itemsToShow[index].fields.title,
+                                    style: const TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(itemsToShow[index].fields.author),
+                                  const SizedBox(height: 10),
+                                  Text(itemsToShow[index]
+                                              .fields
+                                              .description
+                                              .length >
+                                          200
+                                      ? "${itemsToShow[index].fields.description.substring(0, 201)}..."
+                                      : itemsToShow[index].fields.description),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                      "Banyak Halaman: ${itemsToShow[index].fields.numPages}"),
+                                ],
+                              )
+                                )
+                                )
+                            )
+                            ),
                           ),
-                        ),
-                      ),
-                    );
-                  }),
-            ),
-          ],
-        ));
+                        );
+                      }),
+                ),
+              ],
+            )));
   }
 }
